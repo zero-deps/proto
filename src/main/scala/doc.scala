720f344d65b45f219dae16f3c21e59683392e00d
Andrii Nemchenko <a.nemchenko@icloud.com>
2020-03-04 18:46:42 +0200

    better tex

--- src/main/scala/doc.scala
+++ src/main/scala/doc.scala
@@ -6,22 +6,26 @@ import zd.gs.z._
 object Doc {
   type Version = String
   type Change = String
-  type ChangeLog = Seq[(Version, Seq[String])]
+  type ChangeLog = String
   def tex(xs: Seq[Tpe]): (String, ChangeLog) = {
     val changeLog = xs.flatMap{ x =>
-      since(x.tpe.typeSymbol).map{ case (x1, y) => x1 -> s"${x.name}: $y" }
-    }.groupBy(_._1).to(List).sortBy(_._1).reverse.map{ case (x, y) => x -> y.map(_._2).sorted}
+      since(x.tpe.typeSymbol).map{ case (x1, y) => x1 -> s"  \\item{${x.name}} $y" }
+    }.groupBy(_._1).to(List).sortBy(_._1).reverse.map{ case (x, y) => s"\\subsubsection{$x}" -> y.map(_._2).sorted.mkString("\n")}.map{
+      case (x, "") => x
+      case (x, y) => x + "\n\\begin{itemize}\n" + y + "\n\\end{itemize}"
+    }.mkString("\n")
     xs.flatMap{
+      case x: TraitType if x.firstLevel => Nothing
       case x: TraitType =>
-        val s = x.name + " := " + x.children.map(_.name).mkString(" | ")
-        List(s).just
+        val a = x.children.map(y => s"  \\item ${y.name}").mkString("\n")
+        val b = s"\\paragraph{${x.name}}\n" + (if (a.nonEmpty) s"\\begin{itemize}\n$a\n\\end{itemize}" else throw new Exception("no children: check @N on children"))
+        b.just
       case x @ (_: RegularType | _: RecursiveType | _: NoargsType) =>
-        since(x.tpe.typeSymbol)
-        (s"Fields for ${x.name}:" +:
-          fields(x.tpe).map(y => y._1 + ": " + pursTypePars(y._2)._1)
-        ).just
+        val a = fields(x.tpe).map(y => s"  \\item[${y._1}] ${pursTypePars(y._2)._1}").mkString("\n")
+        val b = if (a.nonEmpty) s"\\paragraph{${x.name}}\n\\begin{description}\n${a}\n\\end{description}" else s"\\paragraph{${x.name}} No fields"
+        b.just
       case x: TupleType => Nothing
-    }.flatten.mkString("\n") -> changeLog
+    }.mkString("\n") -> changeLog
   }
   
   private[this] def since(x: Symbol): Seq[(Version,Change)] = {
--- src/test/scala/doc.test.scala
+++ src/test/scala/doc.test.scala
@@ -11,24 +11,55 @@ class DocTest extends AnyFreeSpec with Matchers {
   val res = Purescript.generate[Push, Pull](moduleEncode="DocTest.Pull", moduleDecode="DocTest.Push", moduleCommon="DocTest.Common", codecs=tc::Nil)
   "doc" - {
     "pull" in {
-      res.doc.collectFirst{ case ("DocTest.Pull", x) => x._1 }.get shouldBe """Pull := Ping | Ask
-        |Fields for Ping:
-        |Fields for Ask:
-        |what: String""".stripMargin
+      res.doc.collectFirst{ case ("DocTest.Pull", x) => x._1 }.get shouldBe
+      """\paragraph{Ping} No fields
+        |\paragraph{Ask}
+        |\begin{description}
+        |  \item[what] What
+        |\end{description}
+        |\paragraph{What}
+        |\begin{itemize}
+        |  \item Watt
+        |  \item Who
+        |\end{itemize}
+        |\paragraph{Watt} No fields
+        |\paragraph{Who}
+        |\begin{description}
+        |  \item[asks] String
+        |\end{description}""".stripMargin
     }
     "push" in {
-      res.doc.collectFirst{ case ("DocTest.Push", x) => x._1 }.get shouldBe """Push := Ping | Translated
-        |Fields for Ping:
-        |Fields for Translated:
-        |xs: (Array (Tuple String String))""".stripMargin
+      res.doc.collectFirst{ case ("DocTest.Push", x) => x._1 }.get shouldBe
+      """\paragraph{Ping} No fields
+        |\paragraph{Translated}
+        |\begin{description}
+        |  \item[xs] (Array (Tuple String String))
+        |\end{description}""".stripMargin
     }
   }
   "log" - {
     "pull" in {
-      res.doc.collectFirst{ case ("DocTest.Pull", x) => x._2 }.get shouldBe Seq(("1.0.1", Seq("Ping: added")))
+      res.doc.collectFirst{ case ("DocTest.Pull", x) => x._2 }.get shouldBe
+        """\subsubsection{1.0.1}
+          |\begin{itemize}
+          |  \item{Ping} added
+          |\end{itemize}""".stripMargin
     }
     "push" in {
-      res.doc.collectFirst{ case ("DocTest.Push", x) => x._2 }.get shouldBe Seq(("1.2.0", Seq("Translated: change type of 'xs'")), ("1.1.0", Seq("Translated: rename 'values' to 'xs'")), ("1.0.1", Seq("Ping: added", "Translated: added")))
+      res.doc.collectFirst{ case ("DocTest.Push", x) => x._2 }.get shouldBe
+        """\subsubsection{1.2.0}
+          |\begin{itemize}
+          |  \item{Translated} change type of 'xs'
+          |\end{itemize}
+          |\subsubsection{1.1.0}
+          |\begin{itemize}
+          |  \item{Translated} rename 'values' to 'xs'
+          |\end{itemize}
+          |\subsubsection{1.0.1}
+          |\begin{itemize}
+          |  \item{Ping} added
+          |  \item{Translated} added
+          |\end{itemize}""".stripMargin
     }
   }
 }
@@ -37,7 +68,10 @@ sealed trait Push
 sealed trait Pull
 @Since("1.0.1", "added")
 @N(1) final case object Ping extends Push with Pull
-@N(2) final case class Ask(@N(1) what: String) extends Pull
+@N(2) final case class Ask(@N(1) what: What) extends Pull
+sealed trait What
+@N(1) final case object Watt extends What
+@N(2) final case class Who(@N(1) asks: String) extends What
 @Since("1.2.0", "change type of 'xs'")
 @Since("1.1.0", "rename 'values' to 'xs'")
 @Since("1.0.1", "added")
