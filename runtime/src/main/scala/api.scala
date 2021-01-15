7da90381c80f3d269abdeae99f20c84db34d8740
Andrii Nemchenko <a.nemchenko@icloud.com>
2021-01-15 15:10:54 +0200

    bytes support in api

--- runtime/src/main/scala/api.scala
+++ runtime/src/main/scala/api.scala
@@ -24,12 +24,24 @@ object api {
     bytes
   }
 
-  def decode[A](xs: Array[Byte], offset: Int=0)(implicit c: MessageCodec[A]): A = {
+  def encodeToBytes[A](a: A)(implicit c: MessageCodec[A]) =
+    Bytes.unsafeWrap(encode[A](a))
+
+  def decode[A](xs: Array[Byte])(implicit c: MessageCodec[A]): A =
+    decode[A](xs, offset=0)
+
+  def decode[A](xs: Array[Byte], offset: Int)(implicit c: MessageCodec[A]): A = {
     val is =
       if (offset > 0) CodedInputStream.newInstance(xs, offset, xs.length-offset)
       else CodedInputStream.newInstance(xs)
     c.read(is)
   }
+
+  def decode[A](bs: Bytes)(implicit c: MessageCodec[A]): A =
+    decode[A](bs.unsafeArray)
+
+  def decode[A](bs: Bytes, offset: Int)(implicit c: MessageCodec[A]): A =
+    decode[A](bs.unsafeArray, offset)
 
   final case class N(n: Int) extends annotation.StaticAnnotation
   final case class RestrictedN(nums: Int*) extends annotation.StaticAnnotation
